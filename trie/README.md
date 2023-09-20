ae4bdc01705083c5d28cb6282a4010f29fe3a7e3
Sneha <55897319+sneha-afk@users.noreply.github.com>
2023-09-19 22:50:53 -0700

    add trie notes

---
+++ trie/README.md
@@ -0,0 +1,18 @@
+# trie
+
+A [trie](https://en.wikipedia.org/wiki/Trie) is "a form of string-indexed look-up data structure." 
+
+## Complexity
+
+| Category | Complexity |
+|----------|------------|
+| Time     | $O(n)$     |
+| Space    | $O(n)$     |
+| Insert   | $O(n)$     |
+
+
+## Uses
+* Efficient for storing words and prefixes as each node determines a new pathway for words/strings to form
+
+## Relevant Problems
+* [LC #208](https://leetcode.com/problems/implement-trie-prefix-tree/description/): Implement Trie (Prefix Tree)
---
+++ trie/py_trie.py
@@ -0,0 +1,63 @@
+# Taken from LC #208
+class TrieNode:
+    def __init__(self) -> None:
+        # 26 letters, assumed to be all lowercase
+        self.chars = [None] * 26
+
+        # Is this the last character?
+        self.is_end = False
+
+
+class Trie:
+    root: TrieNode
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    # "void insert(String word) Inserts the string word into the trie"
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            ch_num = ord(ch) - ord("a")
+
+            # Add the letter if it doesn't exist yet
+            if not curr.chars[ch_num]:
+                curr.chars[ch_num] = TrieNode()
+
+            # Move to current character's node
+            curr = curr.chars[ch_num]
+
+        # Finished a word
+        curr.is_end = True
+
+    # "boolean search(String word) Returns true if the string word
+    # is in the trie (i.e., was inserted before), and false otherwise"
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            ch_num = ord(ch) - ord("a")
+
+            # Does the character exist?
+            if not curr.chars[ch_num]:
+                return False
+
+            curr = curr.chars[ch_num]
+
+        # Does the word end?
+        return curr.is_end
+
+    # "boolean startsWith(String prefix) Returns true if there is a previously
+    # inserted string word that has the prefix prefix, and false otherwise"
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            ch_num = ord(ch) - ord("a")
+
+            # Does the character exist?
+            if not curr.chars[ch_num]:
+                return False
+
+            curr = curr.chars[ch_num]
+
+        # All characters accounted for?
+        return True
